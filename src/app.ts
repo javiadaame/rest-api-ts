6369482066792e244d23d2fc578e33cbdc2efdc7
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-12-01 00:24:38 +0100

    feat(app): create and listen app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import "dotenv/config"
+
+import express from "express"
+import cors from "cors"
+
+const PORT = process.env.PORT || 3001;
+
+const app = express()
+app.use(cors())
+
+app.listen(PORT, () => {
+    console.log(`[READY] Launching in port ${PORT}`)
+})
