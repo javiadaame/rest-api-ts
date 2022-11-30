c4ca93a3d699e4b67cef1fd1600ce1a26bbd0806
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-12-01 00:55:38 +0100

    feat(database): created database connection.

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,18 @@ import express from "express"
 import cors from "cors"
 
 import { router } from "./routes";
+import db from './config/mongo'
 
 const PORT = process.env.PORT || 3001;
 
 const app = express()
+
+// Load database
+db().then(() => console.log('[DATABASE] Connection successful.'))
+
 app.use(cors())
 app.use(router);
+
 app.listen(PORT, () => {
     console.log(`[READY] Launching in port ${PORT}`)
 })
