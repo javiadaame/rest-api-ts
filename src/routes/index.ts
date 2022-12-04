cbdb3671ef407232cff7fe67ffb81144a4612acc
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-12-04 21:41:08 +0100

    chore: Change dev comments

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,13 +4,18 @@ import { readdirSync } from "fs"
 const PATH_ROUTER = `${__dirname}`
 const router = Router();
 
-// Clean .ts of file names
+
+/**
+ * Clean the extension file
+ * @param fileName string - File name
+ * @return string - File name without extension 
+ */
 const cleanFileName = (fileName: string) => {
     const file = fileName.split('.').shift()
     return file
 }
 
-// Get file names of folder, and use it
+/* Get file names of folder, and use it */
 readdirSync(PATH_ROUTER).filter((fileName) => {
     const cleanName = cleanFileName(fileName)
 
--- src/utils/error.handle.ts
+++ src/utils/error.handle.ts
@@ -1,5 +1,10 @@
 import { Response } from "express";
 
+/**
+ * Show Error
+ * @param res Response
+ * @param error string - The error
+ */
 const handleHttp = (res: Response, error: string) => {
     res.status(500).send({ error })
 }
