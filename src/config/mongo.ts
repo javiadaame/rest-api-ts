0f3609d824e5f63f8808dd00194cfd0ce9c76240
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-12-05 02:34:46 +0100

    refactor: remove unused thing in mongo

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,9 +2,7 @@ import "dotenv/config"
 import { connect } from "mongoose"
 
 /* Connect to database */
-async function dbConnect(): Promise<void> {
+export default async function dbConnect(): Promise<void> {
     const DB_URI = <string>process.env.DB_URI
     await connect(DB_URI)
 }
-
-export default dbConnect;
